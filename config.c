f6635e3f19e50694f8913e95c4f84768015f31de
lucasmodin <lumo0002@stud.kea.dk>
2025-10-28 13:26:00 +0100

    finally done - just need to remove whitespace in the setting converter method

--- config.c
+++ config.c
@@ -64,18 +64,40 @@ struct config_t *read_config(char *filename) {
 struct setting_t *setting_converter(char *line) {
     // TODO: Allokér en struct setting_t (vha. malloc())
     struct setting_t *setting = malloc(sizeof(struct setting_t));
+    if (!setting) {
+        return NULL; //safe guard
+    }
 
-    // Jeg skal bygge en lille parser der laver "name = Simon" om til to strings.
-    // Loop hen over pladserne i `line` indtil du støder på et `=`. Så ved du at
-    // name-delen slutter og at value-delen begynder. Skal man lave to allokeringer
-    // eller findes der en måde at genbruge `line`? Der findes flere løsninger, og
-    // det er din opgave at vælge en løsning her.
+    int eq_index = -1; //
 
-    // TODO: Find navnet på setting'en i *line
-    // TODO: Find værdien på setting'en i *line
+    for (int i = 0; line[i] != '\0'; i++) {
+        if (line[i] == '=') {
+            eq_index = i;
+            break;
+        }
+    }
 
-    setting->name = NULL; // skal ændres
-    setting->value = NULL; // skal ændres
+    if (eq_index == -1) {
+        return NULL; //hvis der ikke er et '=' tegn, så skal vi returnere null;
+    }
+
+    line[eq_index] = '\0'; //vi splitter strengen ved '=' ved at sætte en nul-byte
+
+    char *left = line;
+    char *right = &line[eq_index + 1];
+
+    setting->name = malloc(strlen(left) + 1);
+    setting->value = malloc(strlen(right) + 1);
+
+    if (!setting->name || !setting->value) {
+        free(setting->name);
+        free(setting->value);
+        free(setting);
+        return NULL; //endnu en safe guard hvis allokering fejler
+    }
+
+    strcpy(setting->name, left);
+    strcpy(setting->value, right);
 
     // TODO: return den setting hvor felterne er sat
     return setting;
@@ -83,12 +105,13 @@ struct setting_t *setting_converter(char *line) {
 
 void print_setting(struct setting_t *setting) {
     // TODO: Print en enkelt setting's name og value
+    printf("%s = %s\n", setting->name, setting->value);
 }
 
 void print_config(struct config_t *config) {
     for (int i = 0; i < config->count; i++) {
-        printf("Setting: %s", config->lines[i]);
-        // print_setting(config->lines[i]);
+        struct setting_t *setting = setting_converter(config->lines[i]);
+        print_setting(setting);
     }
 }
 
